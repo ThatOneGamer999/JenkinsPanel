2a9b0f83410378238613f33b014cacbc9e24e638
ThatOneGamer999 <keenonsalish@icloud.com>
2017-02-15 05:39:17 +0800

    Update main.js

--- build-windows/resources/app/main.js
+++ build-windows/resources/app/main.js
@@ -24,6 +24,7 @@ function createWindow () {
 
   // Open the DevTools.
 //mainWindow.webContents.openDevTools()
+  //note in order to compile
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
